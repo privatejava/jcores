b4f5ed34a05352dee4ba57159c8343074aa37115
Ralf Biedert <devnull@localhost>
2011-08-29 18:10:09 +0200

    Added extension mechanism.

--- core/jre/tests/src/junit/ExtensionsTest.java
+++ core/jre/tests/src/junit/ExtensionsTest.java
@@ -83,7 +83,6 @@ public class ExtensionsTest {
     @Test
     public void testSingleton() {
         final String random = $(S.class).random();
-        
         Assert.assertEquals(random, $(S.class).random());
     }
 
